bff2bf337bbc9de897f6947628cec9005b34db1f
Yaroslav <yaroslav00demchenko>
2018-12-25 14:32:52 +0200

    chaos game

--- openGL_test/openGL_test.cpp
+++ openGL_test/openGL_test.cpp
@@ -4,79 +4,70 @@
 #include "pch.h"
 #include <iostream>
 #include <GL/glut.h>
+#include <cmath>
+#include <vector>
 
 using namespace std;
 
 const float PI = 3.1415265358979323;
 
+const int HEIGHT = 100;
+const int WIDTH = 100;
+
+class Point {
+public:
+	float x, y;
+	float r, g, b;
+
+	void show(float k) {
+		glColor3f(r,g,b);
+		if (k > 1) {
+			glVertex2f(2 * x / k, 2 * y);
+		}
+		else {
+			glVertex2f(2 * x, 2 * y * k);
+		}
+	}
+
+	Point() {
+		x = 0.0;
+		y = 0.0;
+		r = 1.0;
+		g = 1.0;
+		b = 1.0;
+	}
+
+	Point(float x_, float y_) : Point() {
+		x = x_;
+		y = y_;
+	}
+
+	Point(float x_, float y_, float r_, float g_, float b_) : Point(x_, y_) {
+		r = r_;
+		g = g_;
+		b = b_;
+	}
+};
+vector<Point> points;
+
+int dist(int a, int b, int n) {
+	int dif = abs(a - b);
+	if (dif < (-dif + n)) {
+		return dif;
+	}
+	return -dif + n;
+}
+
 void displayCB(void)		/* function called whenever redisplay needed */
 {
 	glClear(GL_COLOR_BUFFER_BIT);		/* clear the display */
-	glClearColor(0.0, 0.0, 0.0, 0);
-	glColor3f(1.0, 1.0, 1.0);
-
-	float ax, ay, bx, by, cx, cy, dx,dy,ex,ey;
-	float x, y;
-
-	glPointSize(0.1);
+	glClearColor(0.01, 0.02, 0.03, 0);
+	glPointSize(0.01);
+	float k = glutGet(GLUT_WINDOW_WIDTH) / float(glutGet(GLUT_WINDOW_HEIGHT));	
 	glBegin(GL_POINTS);
-
-	int n = 5;
-
-	ax = cos(2 * PI / n);
-	ay = sin(2 * PI / n);
-
-	bx = cos(2*2 * PI / n);
-	by = sin(2*2 * PI / n);
-
-	cx = cos(3 * 2 * PI / n);
-	cy = sin(3 * 2 * PI / n);
-
-	dx = cos(4 * 2 * PI / n);
-	dy = sin(4 * 2 * PI / n);
-
-	ex = cos(5 * 2 * PI / n);
-	ey = sin(5 * 2 * PI / n);
-
-	x = (rand() % 200 - 100) / 100.0;
-	y = (rand() % 200 - 100) / 100.0;
-
-	int p, prev = -1;
-
-	for (int j = 0; j < 100; j++) {
-		for (int i = 0; i < 10000; i++) {
-			glColor3f(1.0, 0.0, 0.0);
-			glVertex2f(x, y);
-
-			p = rand() % n;
-			if (p == prev) {
-				continue;
-			}
-
-			if (p == 0) {
-				x = (ax + x) * 0.5;
-				y = (ay + y) * 0.5;
-			}
-			else if (p == 1) {
-				x = (bx + x) * 0.5;
-				y = (by + y) * 0.5;
-			}
-			else if (p == 2) {
-				x = (cx + x) * 0.5;
-				y = (cy + y) * 0.5;
-			}
-			else if (p == 3) {
-				x = (dx + x) * 0.5;
-				y = (dy + y) * 0.5;
-			}
-			else if (p == 4) {
-				x = (ex + x) * 0.5;
-				y = (ey + y) * 0.5;
-			}
-
-			prev = p;
+		for (auto point : points) {
+			point.show(k);
 		}
-	}
 	glEnd();	
 	glFlush();
 }
@@ -86,16 +77,92 @@ void keyCB(unsigned char key, int x, int y)	/* called on key press */
 	if (key) exit(0);
 }
 
+
+void createFractal() {
+	const int n = 5;
+	float percent = 0.5;
+	float rotation = -PI / 6;
+	Point basePoints[n];
+	Point main = Point((rand() % 200 - 100) / 100.0, (rand() % 200 - 100) / 100.0, 0.0, 0.0, 0.0);
+
+	for (int i = 0; i < n; i++) {
+		basePoints[i].x = cos(i * 2 * PI / n );
+		basePoints[i].y = sin(i * 2 * PI / n );
+	}
+	points.resize(0);
+
+	int p, prev1 = -1, prev2 = -1, prev3 = -1;	
+
+	for (int j = 0; j < 100; j++) {
+		for (int i = 0; i < 10000; i++) {
+
+			p = rand() % n;
+			if (dist(p, prev1, n) == 0) {
+				continue;
+			}
+
+			main.x = (basePoints[p].x + main.x) * percent;
+			main.y = (basePoints[p].y + main.y) * percent;
+
+			switch (p) {
+			case 0:
+				main.r = 1.0;
+				main.g = 0.0;
+				main.b = 0.0;
+				break;
+			case 1:
+				main.r = 0.0;
+				main.g = 1.0;				
+				main.b = 0.0;
+				break;
+			case 2:
+				main.b = 1.0;
+				main.r = 0.0;
+				main.g = 0.0;
+				break;
+			case 3:
+				main.r = 1.0;
+				main.g = 1.0;
+				main.b = 0.0;
+				break;
+			case 4:
+				main.r = 1.0;
+				main.g = 0.0784;
+				main.b = 0.57647;
+				break;
+			case 5:
+				main.r = 1.0;
+				main.g = 0.0;
+				main.b = 1.0;
+				break;
+			default:
+				main.r = 1.0;
+				main.g = 1.0;
+				main.b = 1.0;
+			}
+
+			points.push_back(main);
+
+			prev2 = prev1;
+			prev1 = p;			
+		}
+	}
+
+
+}
+
+
 int main(int argc, char *argv[])
 {
 
 	glutInit(&argc, argv);		/* initialize GLUT system */
 	glutInitDisplayMode(GLUT_RGB);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(600, 600);		/* width=400pixels height=500pixels */
 	glutCreateWindow("Triangle");	/* create window */
+	glutFullScreen();
 
 	/* from this point on the current window is win */
+
+	createFractal();
 
 	glutDisplayFunc(displayCB);		/* set window's display callback */
 	glutKeyboardFunc(keyCB);		/* set window's key callback */
